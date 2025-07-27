0269ca3997cf679b56980f5d7ea63a4a6c1773f3
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-27 20:40:34 +0500

    Annotations added

---
+++ 1) Annotation/index.ts
@@ -0,0 +1,20 @@
+// String
+// let str: string = "Hello, World!";
+
+let myName: string = "John Doe";
+console.log(myName);
+
+myName = "Something other person";
+console.log(myName);
+
+// Number
+// let num: number = 42;
+
+let myAge: number = 30;
+console.log(myAge);
+
+// Boolean
+// let isActive: boolean = true;
+
+let tsHard: boolean = false;
+console.log(tsHard);
