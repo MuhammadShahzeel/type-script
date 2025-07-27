575c3c781c76ff2e55fa9d4de5076633c727fa31
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-27 23:32:13 +0500

    Multidimensional Array

---
+++ 8) Multidimensional Array/index.ts
@@ -0,0 +1,20 @@
+const singleDimensionalArray: number[] = [1, 2, 3, 4, 5];
+const multiDimensionalArray: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+const tripleDimensionalArray: number[][][] = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+
+console.log("Single Dimensional Array:", singleDimensionalArray);
+console.log("Multi Dimensional Array:", multiDimensionalArray);
+console.log("Triple Dimensional Array:", tripleDimensionalArray);
+
+// Also write as
+const anotherSingleDimensionalArray: number [] = [10, 20, 30];
+const anotherMultiDimensionalArray: number[][] = [[10,20,30,40]];
+const anotherTripleDimensionalArray: number[][][] = [[[10,20,30,40,50,60]]];
+
+console.log("Another Single Dimensional Array:", anotherSingleDimensionalArray);
+console.log("Another Multi Dimensional Array:", anotherMultiDimensionalArray);
+console.log("Another Triple Dimensional Array:", anotherTripleDimensionalArray);
