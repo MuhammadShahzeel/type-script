b0ccf609e311201039db149bb184a87b584032e2
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-27 20:56:03 +0500

    any type

---
+++ 3) Any Type/index.ts
@@ -0,0 +1,10 @@
+// ANy type use bht kam hoti ha bs isko ap use krskty ek he variable ma aur usko string bh deskty int aur boolean bh 
+
+let color: any = "red";
+console.log(color); // Output: red
+
+color = 5;
+console.log(color); // Output: 5
+
+color = true;
+console.log(color); // Output: true
