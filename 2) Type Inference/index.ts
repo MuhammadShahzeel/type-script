f20636f790767e0c231f70cb4507d9570fce8c7c
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-27 20:51:29 +0500

    Type inferencing

---
+++ 2) Type Inference/index.ts
@@ -0,0 +1,11 @@
+// Type infering ma hamay data type batane ki need nh annotation ki tarah
+
+let tech = "TypeScript";
+//tech = 12;  this will cause an error but if you javascript it will work
+console.log(tech);
+
+let favNumber = 7;
+console.log(favNumber);
+
+let isLearning = true;
+console.log(isLearning);
