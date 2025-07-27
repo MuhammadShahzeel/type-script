b150f5315f01c69c3a0215f0e509d13fb58405da
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-27 22:31:39 +0500

    Void function

---
+++ 5) Void/index.ts
@@ -0,0 +1,11 @@
+function printMessage(message: string): void {
+    console.log(message);
+}
+printMessage("This is a void function example.");
+
+// acha hum direct bh likhskty hai void likhne ki need nh code khud samjhjayega ke void function hai kiunke return nh likhrhy
+
+function logMessage(msg: string) {
+    console.log(msg);
+}
+logMessage("Logging a message without return type.");
