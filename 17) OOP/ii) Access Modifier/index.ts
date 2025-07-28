b49718019e0777b2519229cb0fa1a18ec4393d6f
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-29 02:09:14 +0500

    access modifier

---
+++ 17) OOP/ii) Access Modifier/index.ts
@@ -0,0 +1,12 @@
+/* Public
+Members marked as public can be accessed from anywhere, both inside and outside the class
+*/
+
+/* Private
+Members marked as private can only be accessed with in the class they are defined in
+*/
+
+/* Protected
+Members marked as protected can  be accessed from with in the class they are defined in,as well as any subclasses that extend the class
+*/
+
