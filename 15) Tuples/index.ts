265ded807e55c402259ada097c7646e02a213320
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-29 01:26:08 +0500

    tuples

---
+++ 15) Tuples/index.ts
@@ -0,0 +1,11 @@
+// array ke taarh he hota hai bs isma array ke ander data type define karna hota hai
+// jo data type pattern ma define kiya hota hai wo hi data type isma store ho sakta hai
+let myTuple: [string, number, boolean] = ["John", 30, true];
+console.log(myTuple[0]); // Output: John
+console.log(myTuple[1]); // Output: 30      
+console.log(myTuple[2]); // Output: true
+console.log(myTuple.length); // Output: 3
+console.log(myTuple);
+
+
+
