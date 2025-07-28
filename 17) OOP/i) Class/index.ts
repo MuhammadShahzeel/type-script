071cadb5d2a9490f2f3ba4ca7da019fe53f341c1
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-07-29 01:46:28 +0500

    class

---
+++ 17) OOP/i) Class/index.ts
@@ -0,0 +1,12 @@
+class Person1 {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const person1 = new Person1("Alice", 30);
+console.log(person1);
+
