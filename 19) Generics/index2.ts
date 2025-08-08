ed738a575a8a812b0c9b822b8aa342130ba9e5c8
Muhammad Shahzeel <146673040+MuhammadShahzeel@users.noreply.github.com>
2025-08-09 01:04:12 +0500

    Update index2.ts

--- 19) Generics/index2.ts
+++ 19) Generics/index2.ts
@@ -37,7 +37,74 @@ interface Dog{
     name: string;
     breed: string;
 }
+
+
+
 const dog1 = UniqueDataTypesFunc<Dog>({name: "Jeffy", breed: "persian"},{name: "Waju", breed: "badmash"})
 console.log(dog1);
+
+// another example
+// Generic function jisme do type parameters hain: T aur U
+function reversePair<T, U>(value1: T, value2: U): [U, T] {
+    // Function dono values ko ulta karke tuple mein return karta hai
+    return [value2, value1];
+}
+
+// Function ko call kiya jahan pehla value string hai aur doosra number
+const reversedPair = reversePair("hello", 20);
+
+// Output hoga: [20, "hello"]
+console.log(reversedPair);
+
+
+
+// --------------------------------------
+//generic class
+// Generic class Box jisme type T pass hoti hai
+class Box<T> {
+
+    private content: T;
+    constructor(initalContent: T) {
+        this.content = initalContent;
+    }
+    getContent(): T {
+        return this.content;
+    }
+    setContent(newContent: T): void {
+        this.content = newContent;
+    }
+}
+// Yahan Box class ka ek instance banaya gaya with string type
+const stringBox = new Box<string>("Hello, TypeScript");
+console.log(stringBox.getContent());  // Output: Hello, TypeScript
+// Number type box
+const numberBox = new Box<number>(123);
+console.log(numberBox.getContent()); // Output: 123
+
+// Set new number value
+numberBox.setContent(456);
+console.log(numberBox.getContent()); // Output: 456
+
+// String type box again
+const anotherStringBox = new Box<string>("Generic Power");
+console.log(anotherStringBox.getContent()); // Output: Generic Power
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
